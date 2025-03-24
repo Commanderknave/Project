9c6c3a338d860c810c50c5cd1d444db6131d8d5d
Luc <LukeLosier@outlook.com>
2025-03-24 17:45:34 -0300

    Remove Friends

--- Project/sql/friends.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS friends;
-
-CREATE TABLE friends(
-	friender INT,
-    friendee INT,
-    FOREIGN KEY (friender) REFERENCES users (user_id),
-    FOREIGN KEY (friendee) REFERENCES users (user_id),
-    PRIMARY KEY (friender,friendee)
-)
